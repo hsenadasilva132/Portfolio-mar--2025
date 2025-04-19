eed8f1056115db6f0ff8125cec874a2f6b7424bd
Henrique Sena <135704597+hsenadasilva132@users.noreply.github.com>
2025-04-19 12:33:29 -0300

    Animação de Scroll
    Adicionado animação ao rolar a tela para baixo ativa a exibição dos elementos

--- script.js
+++ script.js
@@ -15,3 +15,31 @@ links.forEach(link => {
         });
     });
 });
+
+//Função para detectar se o elemento esta visivel na tela 
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+// Função para aplicar as animações
+function handleScroll() {
+    const elements = document.querySelectorAll('.element-hidden'); // Seleciona os elementos
+    elements.forEach((element) => {
+        if(isElementInViewport(element)) {
+            element.classList.add('element-visible'); // Adiciona a classe visivel 
+            element.classList.remove('element-hidden'); // remove a clase escondida
+        }
+    })
+}
+
+//Adiciona o evento de rolagem á janela 
+window.addEventListener('scroll', handleScroll);
+
+//Para garantir que elementos já visiveis ao carregar sejam exibidos 
+window.addEventListener('load', handleScroll);

1b7f30685d7425c93d9b468531df3e8d16057151
Henrique Sena <135704597+hsenadasilva132@users.noreply.github.com>
2025-04-23 00:16:30 -0300

    Teste de ajuste de scroll - 7

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-//Seleciona todos os links de navegação 
 const links = document.querySelectorAll('.a-navigation');
 
 links.forEach(link => {
     link.addEventListener('click', event => {
         event.preventDefault(); // Impede o comportamento padrão do clique
 
-        //obtém o destino do link através do atributo href
+        // Obtém o ID da seção de destino
         const targetId = link.getAttribute('href').substring(1);
         const targetSection = document.getElementById(targetId);
 
-        //Rolamento suave
+        // Verifica a seção de destino e ajusta o parâmetro "block" do scroll
+        const scrollBehavior = targetId === 'contato' ? 'end' : 'start';
+
+        // Rolamento suave
         targetSection.scrollIntoView({
-            behavior: 'smooth', //define o comportamento suave 
-            block: 'start'
+            behavior: 'smooth',
+            block: scrollBehavior // Aplica o comportamento específico
         });
     });
 });
